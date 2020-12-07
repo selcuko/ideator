28b2c37896de6a5f4d4410353e24d047e44dc02a
divisia <omrfyyz@gmail.com>
2020-12-07 07:08:20 +0300

    settings.py conf & base.html

--- ideator/settings.py
+++ ideator/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'ideator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = ['assets']
