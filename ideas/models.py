4af9bb9941cefc4f2d742bf24449df5d23786e96
divisia <omrfyyz@gmail.com>
2020-12-07 06:38:59 +0300

    idea model

--- ideas/models.py
+++ ideas/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Idea(models.Model):
+    class Meta:
+        ordering = ('-created_at',)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    external_link = models.URLField(null=True, blank=True)
+    internal_link = models.URLField(null=True, blank=True)
+    submitted_by = models.ForeignKey('auth.User', on_delete=models.SET_NULL, null=True, blank=True)
+    color = models.CharField(max_length=20, null=True, blank=True)
+    magnitude = models.IntegerField(default=1)
+    slug = models.SlugField(editable=False)
+
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, keep_slug=False, **kwargs):
+        if not keep_slug: 
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
